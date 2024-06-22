e4eb88dd10ceba30ce9b8b3b3165cdcb1552c1da
phdangg <dangmanhphuc2003@gmal.com>
2024-06-22 13:35:23 +0700

    two sum

---
+++ src/main/java/Array_Hashing/TwoSum_1.java
@@ -0,0 +1,11 @@
+package Array_Hashing;
+
+public class TwoSum_1 {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 1; i < nums.length; i++){
+            for (int j = i; j < nums.length; j++)
+                if (nums[j] + nums[j-i] == target) return new int[]{j-i, j};
+        }
+        return null;
+    }
+}
