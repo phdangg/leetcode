e821f147cf95c732ad0f4e55bd077144eab600a3
phdangg <dangmanhphuc2003@gmal.com>
2024-06-25 13:58:56 +0700

    product of array except self ver2

--- src/main/java/Array_Hashing/ProductofArrayExSelf_238.java
+++ src/main/java/Array_Hashing/ProductofArrayExSelf_238.java
@@ -18,6 +18,18 @@ public class ProductofArrayExSelf_238 {
             output[i] = prefix[i]*suffix[i];
         return output;
     }
+    public static int[] productExceptSelf2(int[] nums) {
+        int[] output = new int[nums.length];
+        output[0] = 1;
+        for (int i = 1; i < output.length; i++)
+            output[i] = output[i-1]*nums[i-1];
+        int r = 1;
+        for (int i = nums.length - 1; i > 0; i--){
+            output[i] *= r;
+            r *= nums[i];
+        }
+        return output;
+    }
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(productExceptSelf(new int[]{1,2,3,4})));
